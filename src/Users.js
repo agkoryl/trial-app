8e0bbd7bd792541e3d1bbe82f2366b9d1d880371
Agnieszka Koryl <ag.koryl@gmail.com>
2019-02-23 10:15:40 +0100

    users

--- src/Users.js
+++ src/Users.js
@@ -1,11 +1,35 @@
 import React from 'react';
 
+const styles = {
+    fontSize: 40,
+    padding: 20
+}
+
+
 class Users extends React.Component {
 
-    constructor() {
-        this.users = {};
+    users = [
+        {
+            name: 'John',
+            surname: 'Ble'
+        },
+        {
+            name: 'Jane',
+            surname: 'Yellow'
+        }
+    ]
+        ;
+ 
+    render() {
+        return (
+            <div style={styles}>
+                {this.users.map(function (user) {
+                    return <div>{user.name} {user.surname}</div>
+                })}
+            </div>
+        )
     }
-
 }
+
 
 export default Users;
